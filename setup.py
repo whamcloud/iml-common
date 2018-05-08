5a01ce3eb1c29e2dab37dd922f7946ff070f6d9f
Joe Grund <joe.grund@intel.com>
2018-05-07 20:39:51 -0400

    Update manifest to include license + readme (#46)
    Create-Tag: 1.4.1
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 
-
 from setuptools import setup, find_packages
 from iml_common import package_version
 # To use a consistent encoding
@@ -19,21 +18,20 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 excludes = ["*tests*"]
 
 setup(
-    name = 'iml-common',
-    version = package_version(),
-    author = "Intel Corporation",
-    author_email = "iml@intel.com",
-    url = 'https://pypi.python.org/pypi/iml-common',
-    packages = find_packages(exclude=excludes),
-    include_package_data = True,
-    license = 'MIT',
-    description = 'Common library used by multiple IML components',
-    long_description = long_description,
-    classifiers = [
+    name='iml-common',
+    version=package_version(),
+    author="Intel Corporation",
+    author_email="iml@intel.com",
+    url='https://pypi.python.org/pypi/iml-common',
+    packages=find_packages(exclude=excludes),
+    include_package_data=True,
+    license='MIT',
+    description='Common library used by multiple IML components',
+    long_description=long_description,
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
     ],
-    keywords = 'IML lustre high-availability'
-)
+    keywords='IML lustre high-availability')
